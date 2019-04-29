8084dd72308cef12c6aced26e18f71778bc48570
Bhupendra <bdpiparva@gmail.com>
2019-04-29 13:55:28 +0530

    Create example.gocd.groovy

---
+++ example.gocd.groovy
@@ -0,0 +1,35 @@
+GoCD.script {
+  pipelines {
+    pipeline('test-groovy') {
+      group = 'first'
+      materials {
+        git {
+          branch = 'master'
+          shallowClone = false
+          url = 'https://github.com/bdpiparva/config_repo_sample'
+        }
+      }
+      stages {
+        stage('test') {
+          artifactCleanupProhibited = false
+          cleanWorkingDir = false
+          fetchMaterials = true
+          approval {
+          }
+          jobs {
+            job('test') {
+              runInstanceCount = '0'
+              timeout = 0
+              tasks {
+                exec {
+                  commandLine = ['ls', '-alh']
+                  runIf = 'passed'
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
