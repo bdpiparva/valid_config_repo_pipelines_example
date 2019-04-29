37addc892e9099bb26be8a1ab47e6bf4da98fcd9
Bhupendra <bdpiparva@gmail.com>
2019-04-29 14:01:56 +0530

    Update example.gocd.groovy

--- example.gocd.groovy
+++ example.gocd.groovy
@@ -1,27 +1,31 @@
+def all = [1,2,3,4];
+
 GoCD.script {
   pipelines {
-    pipeline('test-groovy') {
-      group = 'first'
-      materials {
-        git {
-          branch = 'master'
-          shallowClone = false
-          url = 'https://github.com/bdpiparva/config_repo_sample'
-        }
-      }
-      stages {
-        stage('test') {
-          artifactCleanupProhibited = false
-          cleanWorkingDir = false
-          fetchMaterials = true
-          approval {
+    all.each { p ->
+      pipeline("test-groovy-$p") {
+        group = 'first'
+        materials {
+          git {
+            branch = 'master'
+            shallowClone = false
+            url = 'https://github.com/bdpiparva/config_repo_sample'
           }
-          jobs {
-            job('test') {
-              tasks {
-                exec {
-                  commandLine = ['ls', '-alh']
-                  runIf = 'passed'
+        }
+        stages {
+          stage('test') {
+            artifactCleanupProhibited = false
+            cleanWorkingDir = false
+            fetchMaterials = true
+            approval {
+            }
+            jobs {
+              job('test') {
+                tasks {
+                  exec {
+                    commandLine = ['ls', '-alh']
+                    runIf = 'passed'
+                  }
                 }
               }
             }
